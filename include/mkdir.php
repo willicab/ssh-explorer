5a18a47dc50df976d493a35b575ad52a5b1e3141
Daniel Gomez <danielmercad@gmail.com>
2017-04-02 16:58:11 -0400

    Agregada funcionalidad para creación de directorio

---
+++ include/mkdir.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include_once 'config.php';
+
+$path = str_replace("//", "/", $config["root"]."/".$_POST["path"]."/".$_POST["file"]);
+
+$command = "mkdir '$path'";
+$ssh->exec($command);
+
+include_once 'cd.php';
