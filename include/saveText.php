6623e9fc0aa778ea6f50da59d71bb8799362d7b8
William Cabrera <cabrerawilliam@gmail.com>
2017-05-02 14:43:41 -0400

    salidas de debug

--- include/saveText.php
+++ include/saveText.php
@@ -9,4 +9,6 @@ $text = str_replace('"', '\"', str_replace('$', '\$', $_POST["text"]));
 
 $command = "echo \"$text\" > '$path'";
 $res = $ssh->exec($command);
-echo json_encode(array("error"=>0, "data"=>array($res), "cmd"=>$command));
+echo $command;
+echo $res;
+//echo json_encode(array("error"=>0, "data"=>array($res), "cmd"=>$command));
